2a41e3215f8395548910a8976ea120cb59383db4
JesseJMa <jesseJMa@gmail.com>
2019-06-08 15:58:35 +0800

    mmmm

--- quickSort.py
+++ quickSort.py
@@ -8,4 +8,5 @@ def qsort(arr):
     small = [i for i in arr if i < pivot_element]
     medium = [i for i in arr if i == pivot_element]
     big = [i for i in arr if i > pivot_element]
+    
     return qsort(small) + medium + qsort(big)
